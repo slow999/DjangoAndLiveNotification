889aeeeb9757cf6386235b1be312a2ba7a3e633c
yiqing-lan <yiqing.lan@periship.com>
2022-07-31 17:35:33 -0500

    move web content to root

--- web/gunicorn.channel.conf.py
+++
@@ -1,5 +0,0 @@
-wsgi_app = 'mysite.asgi:application'
-worker_class = 'uvicorn.workers.UvicornWorker'
-workers = 2
-bind = 'unix:/tmp/gunicorn/gunicorn-channel.sock'
-accesslog = '-'
--- web/sms/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-
-class MessageForm(forms.Form):
-    content = forms.CharField(widget=forms.Textarea, label='Your content')
